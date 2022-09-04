e122f2369719fa40806e0b49e15ab43afdd6fb3d
PHILIP <philophilo037@gmail.com>
2022-09-04 21:55:37 +0300

    Loading dom still

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let rightAns,
 rightNumber = 0,
 falseNumber = 0;
+
 document.addEventListener('DOMContentLoaded', function () {
-    addQuestion();
+    AddQuestion();
 
     eventListeners();
 });
+
+eventListeners = () => {
+    document.querySelector('#check').addEventListener('click', validateAnswer);
+
+}
